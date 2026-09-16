53d56d9065493d58da6c7c00be89caecc48358a8
huy <kieugiahuyttt@gmail.com>
2026-09-16 14:07:35 +0700

    Them menu lua chon

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <thread>
+#include <chrono>
 
 struct student {
     int id;
@@ -61,7 +63,7 @@ struct linkedList {
         int i = 1;
         std::cout << "-----In thong tin sinh vien-----\n";
         while(temp != nullptr) {
-            std::cout << "Thong tin ve sinh vien thu " << i << ": \n"; 
+            std::cout << "***Thong tin ve sinh vien thu " << i << ": \n"; 
             std::cout << "Ma SV: " << temp->data.id << "\n" 
             << "Ho va ten: " << temp->data.name << "\n" 
             << "GPA: " << temp->data.gpa << "\n";
@@ -113,10 +115,70 @@ int main() {
         std::cout << "Nhap GPA: ";
         std::cin >> gpa;
         Node* H = new Node(id, gpa, name);
-        Node* E = new Node(id, gpa, name);
-        A.Insert(H);
-        A.push_back(E);
-        A.Print();
-        A.clearList();
+        A.push_back(H);
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(n * 1500));
+    A.Print();
+    int x = n;
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        int k;
+        std::cout << "-----Chon chuc nang muon su dung-----\n"
+        << "1. Them 1 node vao cuoi ds\n" 
+        << "2. Them 1 node vao dau ds\n"
+        << "3. In ds\n"
+        << "4. Xoa 1 node cuoi ds\n"
+        << "5. Xoa 1 node dau ds\n" 
+        << "6. Thoat\n";
+        std::cout << "Lua chon cua ban: ";
+        std::cin >> k;
+        if(k == 6) {
+            break;
+        }
+        else {
+            if(k == 1 || k == 2) {
+                std::cout << "-----Nhap thong tin sinh vien thu " << x + 1 << "-----\n";
+                int id;
+                double gpa;
+                std::string name;
+                std::cout << "Nhap ID: ";
+                std::cin >> id;
+                std::cout << "Nhap ten: ";
+                std::cin.ignore();
+                getline(std::cin, name);
+                std::cout << "Nhap GPA: ";
+                std::cin >> gpa;
+                Node* H = new Node(id, gpa, name);
+                if(k == 1)  
+                    A.push_back(H);
+                else A.Insert(H);
+                x++;
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+                std::cout << "Them thanh cong!\n";
+            }
+            else if(k == 4) {
+                A.pop();
+                x--;
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+                std::cout << "Xoa thanh cong!\n";
+            }
+            else if(k == 5) {
+                A.erase();
+                x--; 
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+                std::cout << "Xoa thanh cong!\n";   
+            } 
+            else {
+                A.Print();
+                std::this_thread::sleep_for(std::chrono::milliseconds(x * 1500));
+                // char t;
+                // std::cout << "Bam y de tiep tuc n de dung lai!\n";
+                // std::cin >> t;
+                // if(t == 'n' || t == 'N') {
+                //     break;
+                // }
+            } 
+        }
+    }
+    A.clearList();
 }
