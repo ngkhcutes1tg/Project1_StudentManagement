e39e6fe483ab96f52936d98e21b5c2746ac8d470
huy <kieugiahuyttt@gmail.com>
2026-09-16 10:41:14 +0700

    Them struct linked list + node end + xoa node dau, cuoi + constructor Node

--- Main/main.cpp
+++ Main/main.cpp
@@ -10,56 +10,96 @@ struct student {
 struct Node {
    student data;
    Node* next; 
+   Node* prev;
+   Node() {
+    data.id = 0;
+    data.gpa = 0.0;
+    data.name = "";
+   }
+   Node(int i, double g, std::string n) {
+    data.id = i;
+    data.gpa = g;
+    data.name = n;
+   } 
 };
 
-Node* head;
+struct linkedList {
 
-void Insert(int i, double g, std::string n) {
-    Node* temp = new Node;
-    temp->next = nullptr;
-    temp->data.id = i;
-    temp->data.gpa = g;
-    temp->data.name = n;
-    if(head == nullptr) {
-        head = temp;
-        return;
-    }
-    Node* temp2 = head;
-    while(temp2->next != nullptr) {
-        temp2 = temp2->next;
-    }
-    temp2->next = temp;
-}
+    Node* head = nullptr;
+    Node* end = nullptr;
+    
+    void Insert(Node* dl) {
+        dl->next = nullptr;
+        dl->prev = nullptr;
 
-void Print() {
-    Node* temp = head; 
-    int i = 1;
-    std::cout << "-----In thong tin sinh vien-----\n";
-    while(temp != nullptr) {
-        std::cout << "Thong tin ve sinh vien thu " << i << ": \n"; 
-        std::cout << "Ma SV: " << temp->data.id << "\n" 
-        << "Ho va ten: " << temp->data.name << "\n" 
-        << "GPA: " << temp->data.gpa << "\n";
-        temp = temp->next;
-        i++;
+        if(head == nullptr) {
+            head = dl;
+            end = dl;
+            return;
+        }
+        dl->next = head;
+        head->prev = dl;
+        head = dl;
     }
-}
 
-void clearList() {
-    Node* temp = head;
-    while(temp != nullptr) {
-        Node* nextnode = temp->next;
-        delete temp;
-        temp = nextnode;
+    void push_back(Node* dl) {
+        dl->next = nullptr;
+        dl->prev = nullptr;
+
+        if(end == nullptr) {
+            end = dl;
+            head = dl;
+            return;
+        }
+        end->next = dl;
+        dl->prev = end;
+        end = dl;
     }
-    head = nullptr;
-}
+
+    void Print() {
+        Node* temp = head; 
+        int i = 1;
+        std::cout << "-----In thong tin sinh vien-----\n";
+        while(temp != nullptr) {
+            std::cout << "Thong tin ve sinh vien thu " << i << ": \n"; 
+            std::cout << "Ma SV: " << temp->data.id << "\n" 
+            << "Ho va ten: " << temp->data.name << "\n" 
+            << "GPA: " << temp->data.gpa << "\n";
+            temp = temp->next;
+            i++;
+        }
+    }
+    
+    void clearList() {
+        Node* temp = head;
+        while(temp != nullptr) {
+            Node* nextnode = temp->next;
+            delete temp;
+            temp = nextnode;
+        }
+        head = nullptr;
+    }
+
+    void pop() {
+        Node* temp2 = end;
+        Node* temp = end->prev;
+        temp->next = nullptr;
+        end = temp;
+        delete temp2; 
+    }
+
+    void erase() {
+        Node* temp = head;
+        head = head->next;
+        delete temp; 
+    }
+};
 
 int main() {
-    head = nullptr;
     int n;
     std::cout << "Nhap so luong sinh vien: ";
     std::cin >> n;
+    linkedList A;
     for(int i = 0; i < n; ++i) {
         std::cout << "-----Nhap thong tin sinh vien thu " << i + 1 << "-----\n";
         int id;
@@ -72,8 +112,11 @@ int main() {
         getline(std::cin, name);
         std::cout << "Nhap GPA: ";
         std::cin >> gpa;
-        Insert(id, gpa, name);
+        Node* H = new Node(id, gpa, name);
+        Node* E = new Node(id, gpa, name);
+        A.Insert(H);
+        A.push_back(E);
+        A.Print();
+        A.clearList();
     }
-    Print();
-    clearList();
 }
